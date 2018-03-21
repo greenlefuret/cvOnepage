3ed37c3e9d85fd9a284e53eea418027f0da4c0c3
greenLeradounaud <greenleradounaud@gmail.com>
2018-03-21 16:29:08 +0100

    gdvdf

--- Profile.php
+++ Profile.php
@@ -1,12 +1,15 @@
 <?php include 'includes/header.php' ?>
-    <div class="container">
-          <div class="text-center row">
-              <h2 navTitre>PR<span class="jaune">O</span>FILE</h2>
+    <div class="container fond">
+          <div class="text-center">
+              <h3 class="titre">PR<span class="jaune">O</span>FIL</h3>
           </div>
-          <div class="row">
-              <img class="img-fluid" src="img/ptrben.png" alt="photo">
+          <div class="text-center">
+              <img class="img-fluid photoMoi" src="img/moi.jpg" alt="photo">
           </div>
-          <div class="resumeProfile row">
+          <div class="text-center jaune txtLabel">
+            <p>FUTUR DEVELOPPEUR WEB</p>
+          </div>
+          <div class="resumeProfile offset-1 col-10">
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
     </div>
