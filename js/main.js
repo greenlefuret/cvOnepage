92c37f86d24456f528ab6f533ef2998eb143a468
Yellow <yellowaformac@gmail.com>
2018-03-21 11:23:07 +0100

    pull init

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
-function height(bloc){
 
+// Cette fonction permet de check le navigateur sur lequel est ouvert le CV et de s'y adapter.
+function height(bloc){
     var hauteur;
 
-
-
     if( typeof( window.innerWidth ) == 'number' )
-
         hauteur = window.innerHeight;
-
     else if( document.documentElement && document.documentElement.clientHeight )
-
         hauteur = document.documentElement.clientHeight;
-
-    document.getElementById(bloc).style.height = hauteur+"px";
-
+        document.getElementById(bloc).style.height = hauteur+"px";
 }
 
 window.onload = function(){ height("page") };
