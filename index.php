eb94571f82bd0e06cf5286d544a76bf3a3a8f208
greenLeradounaud <greenleradounaud@gmail.com>
2018-03-21 14:50:21 +0100

    fdvfvfvfgsdsvfxngnjgofdkgf,bfdkbfogfdlkbgfdjbkdfbnifdkbnfl,kdfnbkdfnbkfdbnfkdbnkfdbnfkdbnfkbnfdkbnflbnfldkgnfkgnbdfjndfkbdfo

--- index.php
+++ index.php
@@ -12,16 +12,16 @@
              <div id="displayComponent"> <!-- Ici seront liées par une route les différentes pages de notre site. -->
                  <?php include 'introduction.php' ?>
              </div>
-             <div class="navigation">
+             <div class="navigation bgProfile">
                  <?php include 'navProfile.php' ?>
              </div>
-             <div class="navigation">
+             <div class="navigation bgResume">
                 <?php include 'navResume.php' ?>
              </div>
-             <div class="navigation">
+             <div class="navigation bgProjets">
                 <?php include 'navProjets.php' ?>
              </div>
-             <div class="navigation">
+             <div class="navigation bgContact">
                 <?php include 'navContact.php' ?>
              </div>
          </div>
--- navProfile.php
+++ navProfile.php
@@ -1,7 +1,7 @@
 
-      <div class=" photo">
+
       <div class="navDecalage">
           <h2 class="navTitre">PR<span class="jaune">O</span>FILE</h2>
           <p class="italique">Quelques mots a propos de moi</p>
       </div>
-    </div>
+    
