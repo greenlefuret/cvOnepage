11b21d92e04709681677f30aa8ed6be3ad91cffc
greenLeradounaud <greenleradounaud@gmail.com>
2018-03-21 15:27:32 +0100

    bfbdfdf

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Portfolio</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/index.css">
-    </head>
-    <body>
-
+<?php include 'includes/header.php' ?>
          <div id="page">  <!-- Tout ce qui se trouve dans cette div sera visible sans scroll. -->
              <div id="displayComponent"> <!-- Ici seront liées par une route les différentes pages de notre site. -->
                  <?php include 'introduction.php' ?>
@@ -25,11 +16,4 @@
                 <?php include 'navContact.php' ?>
              </div>
          </div>
-
-<script type="text/javascript" src="js/main.js"></script>
-<link rel="stylesheet" href="css/navigation.css">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
-</html>
+<?php include 'includes/footer.php' ?>
