74f601ccd21a7dce44385e60d50ee4a4057f08c9
Yellow <yellowaformac@gmail.com>
2018-03-22 15:18:48 +0100

    push contact

---
+++ projets.php
@@ -0,0 +1,224 @@
+<?php include 'includes/header.php'; ?>
+
+    <!-- Projet n°1 -->
+    <div class="col-12 text-center">
+        <h3 class="titre">Pr<span class="jaune">o</span>jets</h3>
+    </div>
+
+    <div class="col-12 text-center thumbnail">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalProjet1">
+          <img src="img/computer.jpeg" alt="" style="width: 200px">
+        </button>
+        <div class="modal fade" id="modalProjet1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header backJaune">
+                <h5 class="modal-title" id="exampleModalLabel">Projet n°1</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="">
+                  <img src="img/computer.jpeg" style="width:100%;" alt="">
+              </div>
+              <div class="modal-footer text-left backJaune">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <!-- Projet n°2 -->
+    <div class="col-12 text-center thumbnail">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalProjet2">
+          <img src="img/computer.jpeg" alt="" style="width: 200px">
+        </button>
+        <div class="modal fade" id="modalProjet2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header backJaune">
+                <h5 class="modal-title" id="exampleModalLabel">Projet n°2</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="">
+                  <img src="img/computer2.jpg" style="width:100%;" alt="">
+              </div>
+              <div class="modal-footer text-left backJaune">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <!-- Projet n°3 -->
+    <div class="col-12 text-center thumbnail">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalProjet3">
+          <img src="img/computer.jpeg" alt="" style="width: 200px">
+        </button>
+        <div class="modal fade" id="modalProjet3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header backJaune">
+                <h5 class="modal-title" id="exampleModalLabel">Projet n°3</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="">
+                  <img src="img/computer2.jpg" style="width:100%;" alt="">
+              </div>
+              <div class="modal-footer text-left backJaune">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <!-- Projet n°4 -->
+    <div class="col-12 text-center thumbnail">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalProjet4">
+          <img src="img/computer.jpeg" alt="" style="width: 200px">
+        </button>
+        <div class="modal fade" id="modalProjet4" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header backJaune">
+                <h5 class="modal-title" id="exampleModalLabel">Projet n°4</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="">
+                  <img src="img/computer2.jpg" style="width:100%;" alt="">
+              </div>
+              <div class="modal-footer text-left backJaune">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <!-- Projet n°5 -->
+    <div class="col-12 text-center thumbnail">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalProjet5">
+          <img src="img/computer.jpeg" alt="" style="width: 200px">
+        </button>
+        <div class="modal fade" id="modalProjet5" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header backJaune">
+                <h5 class="modal-title" id="exampleModalLabel">Projet n°5</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="">
+                  <img src="img/computer2.jpg" style="width:100%;" alt="">
+              </div>
+              <div class="modal-footer text-left backJaune">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <!-- Projet n°6 -->
+    <div class="col-12 text-center thumbnail">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalProjet6">
+          <img src="img/computer.jpeg" alt="" style="width: 200px">
+        </button>
+        <div class="modal fade" id="modalProjet6" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header backJaune">
+                <h5 class="modal-title" id="exampleModalLabel">Projet n°6</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="">
+                  <img src="img/computer2.jpg" style="width:100%;" alt="">
+              </div>
+              <div class="modal-footer text-left backJaune">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <!-- Projet n°7 -->
+    <div class="col-12 text-center thumbnail">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalProjet7">
+          <img src="img/computer.jpeg" alt="" style="width: 200px">
+        </button>
+        <div class="modal fade" id="modalProjet7" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header backJaune">
+                <h5 class="modal-title" id="exampleModalLabel">Projet n°7</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="">
+                  <img src="img/computer2.jpg" style="width:100%;" alt="">
+              </div>
+              <div class="modal-footer text-left backJaune">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <!-- Projet n°8 -->
+    <div class="col-12 text-center thumbnail">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalProjet8">
+          <img src="img/computer.jpeg" alt="" style="width: 200px">
+        </button>
+        <div class="modal fade" id="modalProjet8" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header backJaune">
+                <h5 class="modal-title" id="exampleModalLabel">Projet n°8</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="">
+                  <img src="img/computer2.jpg" style="width:100%;" alt="">
+              </div>
+              <div class="modal-footer text-left backJaune">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+
+<?php include 'includes/footer.php'; ?>
